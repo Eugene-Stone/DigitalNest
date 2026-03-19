07b5e27b93325ff83d16534905356ea6a679805b
Eugene-Stone <eugenestone.work@gmail.com>
2026-03-19 23:50:44 +0200

    Form component

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,140 @@
+import { useForm } from 'react-hook-form';
+import { request } from '../../api/request';
+
+export default function Form() {
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors, isValid },
+	} = useForm({
+		mode: 'onChange',
+		defaultValues: {
+			fullName: 'Full Name Initial',
+		},
+	});
+
+	async function sendForm(value) {
+		const newFormIncome = new Date().toISOString();
+
+		try {
+			await request('/requests', {
+				method: 'POST',
+				body: JSON.stringify({
+					dateSend: newFormIncome,
+					// eslint-disable-next-line
+					dateSendSt: Date.now(),
+					selectField: value.selectField,
+					fullName: value.fullName,
+					companyPhone: value.companyPhone,
+					email: value.email,
+					textarea: value.textarea,
+					checkboxes: value.checkboxes,
+				}),
+			});
+		} catch (error) {
+			console.log(error.message);
+		}
+	}
+
+	function onSubmit(data) {
+		console.log(JSON.stringify(data));
+		sendForm(data);
+		reset();
+	}
+
+	return (
+		<form onSubmit={handleSubmit(onSubmit)} className="contact-form">
+			<div className="form-title h3-title">
+				Start a conversation about your project and get a free technical infrastructure
+				audit.
+			</div>
+			<div className="fields-box">
+				<div className="field-itm">
+					<div className="cust-sel">
+						<select {...register('selectField')}>
+							<option value="">Select Project Type</option>
+							<option value="web">Web Engineering</option>
+							<option value="cloud">Cloud Infrastructure</option>
+							<option value="audit">Technical Audit</option>
+						</select>
+					</div>
+				</div>
+				<div className="field-itm">
+					<div className="cust-inp">
+						<input
+							{...register('fullName', {
+								required: 'Field required',
+								minLength: {
+									value: 3,
+									message: 'Min length 3 symbols',
+								},
+							})}
+							type="text"
+							// placeholder="Full Name"
+						/>
+					</div>
+					{errors.fullName && (
+						<div className="error-field">
+							{errors.fullName.message || 'Full name is required.'}
+						</div>
+					)}
+				</div>
+				<div className="field-itm">
+					<div className="cust-inp">
+						<input {...register('companyPhone')} placeholder="Company Phone" />
+					</div>
+				</div>
+				<div className="field-itm">
+					<div className="cust-inp">
+						<input {...register('email')} type="email" placeholder="Business E-Mail" />
+					</div>
+					{errors.email && <div className="error-field">{errors.email.message}</div>}
+				</div>
+				<div className="field-itm">
+					<div className="cust-textarea">
+						<textarea
+							{...register('textarea')}
+							cols={40}
+							rows={4}
+							placeholder="Briefly describe your project or tech stack"
+							defaultValue={''}
+						/>
+					</div>
+				</div>
+				<div className="field-itm">
+					<ul className="check-lst">
+						<li>
+							<label className="cust-check">
+								<input
+									type="checkbox"
+									{...register('checkboxes')}
+									defaultValue={'Checkbox 1'}
+								/>
+
+								<span className="wpcf7-list-item-label">Checkbox 1</span>
+							</label>
+						</li>
+						<li>
+							<label className="cust-check">
+								<input
+									type="checkbox"
+									{...register('checkboxes')}
+									defaultValue={'Checkbox 2'}
+								/>
+
+								<span className="wpcf7-list-item-label">Checkbox 2</span>
+							</label>
+						</li>
+					</ul>
+				</div>
+				<div className="btn-form-wrap">
+					<span className={`btn-form ${isValid ? '' : 'disabled'}`}>
+						<input type="submit" />
+						<span>Send</span>
+					</span>
+				</div>
+			</div>
+		</form>
+	);
+}
--- src/sections/Contacts/Contacts.jsx
+++ src/sections/Contacts/Contacts.jsx
@@ -1,49 +1,9 @@
-import { useForm } from 'react-hook-form';
+import Form from '../../components/Form/Form';
 import { useSectionData } from '../../hooks/useSectionData';
-import { request } from '../../api/request';
 import TitleHtml from '../../utils/TitleHtml';
 
 export default function Contacts() {
 	const { section, loading, errorData } = useSectionData('/contacts');
-
-	const {
-		register,
-		handleSubmit,
-		reset,
-		formState: { errors, isValid },
-	} = useForm({
-		mode: 'onChange',
-		defaultValues: {
-			fullName: 'Full Name Initial',
-		},
-	});
-
-	async function sendForm(value) {
-		// const newFormIncome = self.crypto?.randomUUID();
-		const newFormIncome = Date.now();
-
-		try {
-			await request('/requests', {
-				method: 'POST',
-				body: JSON.stringify({
-					dateSend: newFormIncome,
-					selectField: value.selectField,
-					fullName: value.fullName,
-					companyPhone: value.companyPhone,
-					textarea: value.textarea,
-					checkboxes: value.checkboxes,
-				}),
-			});
-		} catch (error) {
-			console.log(error.message);
-		}
-	}
-
-	function onSubmit(data) {
-		console.log(JSON.stringify(data));
-		sendForm(data);
-		reset();
-	}
 
 	// console.log(useForm);
 
@@ -122,111 +82,7 @@ export default function Contacts() {
 						)}
 					</div>
 					<div className="col-lg-6">
-						<form onSubmit={handleSubmit(onSubmit)} className="contact-form">
-							<div className="form-title h3-title">
-								Start a conversation about your project and get a free technical
-								infrastructure audit.
-							</div>
-							<div className="fields-box">
-								<div className="field-itm">
-									<div className="cust-sel">
-										<select {...register('selectField')}>
-											<option value="">Select Project Type</option>
-											<option value="web">Web Engineering</option>
-											<option value="cloud">Cloud Infrastructure</option>
-											<option value="audit">Technical Audit</option>
-										</select>
-									</div>
-								</div>
-								<div className="field-itm">
-									<div className="cust-inp">
-										<input
-											{...register('fullName', {
-												required: 'Field required',
-												minLength: {
-													value: 3,
-													message: 'Min length 3 symbols',
-												},
-											})}
-											type="text"
-											// placeholder="Full Name"
-										/>
-									</div>
-									{errors.fullName && (
-										<div className="error-field">
-											{errors.fullName.message || 'Full name is required.'}
-										</div>
-									)}
-								</div>
-								<div className="field-itm">
-									<div className="cust-inp">
-										<input
-											{...register('companyPhone')}
-											placeholder="Company Phone"
-										/>
-									</div>
-								</div>
-								<div className="field-itm">
-									<div className="cust-inp">
-										<input
-											{...register('email')}
-											type="email"
-											placeholder="Business E-Mail"
-										/>
-									</div>
-									{errors.email && (
-										<div className="error-field">{errors.email.message}</div>
-									)}
-								</div>
-								<div className="field-itm">
-									<div className="cust-textarea">
-										<textarea
-											{...register('textarea')}
-											cols={40}
-											rows={4}
-											placeholder="Briefly describe your project or tech stack"
-											defaultValue={''}
-										/>
-									</div>
-								</div>
-								<div className="field-itm">
-									<ul className="check-lst">
-										<li>
-											<label className="cust-check">
-												<input
-													type="checkbox"
-													{...register('checkboxes')}
-													defaultValue={'Checkbox 1'}
-												/>
-
-												<span className="wpcf7-list-item-label">
-													Checkbox 1
-												</span>
-											</label>
-										</li>
-										<li>
-											<label className="cust-check">
-												<input
-													type="checkbox"
-													{...register('checkboxes')}
-													defaultValue={'Checkbox 2'}
-												/>
-
-												<span className="wpcf7-list-item-label">
-													Checkbox 2
-												</span>
-											</label>
-										</li>
-									</ul>
-								</div>
-								<div className="btn-form-wrap">
-									<span className={`btn-form ${isValid ? '' : 'disabled'}`}>
-										<input type="submit" />
-										<span>Send</span>
-									</span>
-								</div>
-							</div>
-						</form>
+						<Form />
 					</div>
 				</div>
 			</div>
